89578be149ee49217c01e0e6c0ccffb530357997
Nicole Rauch <info@nicole-rauch.de>
2020-11-14 13:10:24 +0100

    Released 0.8 -> increased version number to prepare for the next release.

--- WanikaniExternalDefinition.user.js
+++ WanikaniExternalDefinition.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WaniKani External Definition
 // @namespace    http://www.wanikani.com
-// @version      0.8
+// @version      0.9
 // @description  Get External Definition from Weblio, Kanjipedia
 // @author       NicoleRauch (original script by polv)
 // @match        *://www.wanikani.com/review/session*
